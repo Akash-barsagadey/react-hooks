3f4b87b08017a8eba89ef5b15ec7dd0085fa620e
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-10-23 16:03:04 +0530

    use context in classs component

--- src/context/sample.js
+++ src/context/sample.js
@@ -7,3 +7,22 @@ export const Sample = () => {
   
   return <></>
 }
+
+
+// In class componet
+
+
+// class Sample extends React.Component {
+//   static contextType = RegionalMetaContext
+//   render() {
+//     const {country, data} =this.context;
+//     console.log('data',data)
+//     console.log('country',country)
+//     return (
+//       <></>
+//     );
+//   } 
+// }
+
+
+// export default Sample;
